4e79beeaafc114c65648483229cfcee4e5e5b18b
ardithyseni <ah49393@ubt-uni.net>
2022-12-30 14:35:59 +0100

    backend done

--- server/index.js
+++ server/index.js
@@ -28,9 +28,23 @@ app.post('/', async (req, res) => {
     try {
         const prompt = req.body.prompt;
         const response = await openai.createCompletion({
-            
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0.2,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        })
+        res.status(200).send({
+            bot: response.data.choices[0].text
         })
     } catch (error) {
-        
+        console.log(error);
+        res.status(500).send({ error })
     }
 })
+
+const port = process.env.PORT || 5000;
+
+app.listen(5000, () => console.log(`Server is listening on port http://localhost:${port}`))
